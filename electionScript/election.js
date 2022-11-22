5545286baf2100dbca0ce8a94c68a32c8d0f9362
Adedimeji Sebiotimo <dims95@live.com>
2022-11-22 20:12:21 +0000

    feature: implemented ability to delegate votes and to submit commitments

--- electionScript/election.js
+++ electionScript/election.js
@@ -1,16 +1,16 @@
 import Web3 from 'web3';
 import * as fs from 'fs/promises'
 import * as secp256k1 from '@noble/secp256k1'
-// import BigNumber from "bignumber.js"
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber } from "@ethersproject/bignumber"
 
 const CRYPTO_ADDR = '--cryptoAddr';
 const VOTE_ADDR = '--voteAddr';
 const URL = '--url';
 const CRYPTO_ABI_PATH = './abis/cryptoAbi.json';
 const VOTE_ABI_PATH = './abis/voteAbi.json';
-const PHASE_GAP = 300;
+const PHASE_GAP = 40;
 const WEI_DEPOSIT = '5000000000000000000';
+const states = ['SETUP', 'SIGNUP', 'COMMITMENT', 'VOTE', 'FINISHED']
 
 let web3;
 let rawArgsList;
@@ -24,6 +24,7 @@ let cryptoAbi;
 let voteCon;
 let cryptoCon;
 let delegations = {};
+let deadlines = {};
 let noVotes = [];
 let voters = [];
 
@@ -120,7 +121,7 @@ function setupConnections() {
     cryptoCon = new web3.eth.Contract(JSON.parse(cryptoAbi), flagValues[flags.indexOf(CRYPTO_ADDR)])
 }
 
-//Done using admin account. Set eligible voters and open registration
+//Performed with admin account. Set eligible voters and open registration
 async function initElection() {
     let x = await web3.eth.personal.unlockAccount(admin, "")//unlock Admin account
     let now = Math.floor(Date.now()/1000)//timestamp in seconds
@@ -151,11 +152,13 @@ async function initElection() {
 
     //deadlines from contract
     console.log("CONTRACT DEADLINES")
-    console.log(await voteCon.methods.votersFinishSignupPhase().call())
-    console.log(await voteCon.methods.endSignupPhase().call())
-    console.log(await voteCon.methods.endCommitmentPhase().call())
-    console.log(await voteCon.methods.endVotingPhase().call())
-    console.log(await voteCon.methods.endRefundPhase().call())
+    deadlines.votersFinishSignup = (await voteCon.methods.votersFinishSignupPhase().call()) * 1000
+    deadlines.endSignup = (await voteCon.methods.endSignupPhase().call()) * 1000
+    deadlines.endCommitment = (await voteCon.methods.endCommitmentPhase().call()) * 1000
+    deadlines.endVoting = (await voteCon.methods.endVotingPhase().call()) * 1000
+    deadlines.endRefund = (await voteCon.methods.endRefundPhase().call()) * 1000
+    console.log(deadlines)
+    console.log("")
 }
 
 async function regVoter(voter) {
@@ -226,6 +229,136 @@ async function registerVoters() {
 
 async function registerDelegations() {
     //for each delegator, register delegation   
+    let delegators = Object.keys(delegations)
+
+    for (const delegator of delegators) {
+        let delegatee = delegations[delegator]
+        let delegatorLogin = await web3.eth.personal.unlockAccount(delegator, "");
+        console.log(`Delegator ${delegator} logged in : ${delegatorLogin}`)
+
+        let isDelSuccessful = await voteCon.methods.delegate(delegatee).call({from: delegator});
+        if (isDelSuccessful) {
+            await voteCon.methods.delegate(delegatee).send({from: delegator});
+            console.log(`Delegator ${delegator} has delegated vote to ${delegatee}`)
+            console.log("")
+        } else throw `Delegator ${delegator} could not delegate vote to ${delegatee}`
+    }
+}
+
+async function finishRegistration() {
+    //todo admin closes registration once voter registration deadline is reached
+    await web3.eth.personal.unlockAccount(admin, "")//unlock Admin account
+
+    let isRegistrationEnded = await voteCon.methods.finishRegistrationPhase().call({from: admin})
+    console.log(`Is registration ended: ${isRegistrationEnded}`)
+    if(isRegistrationEnded) {
+        await voteCon.methods.finishRegistrationPhase().send({from: admin, gas: 4200000})
+        console.log('Admin has ended registration')
+        console.log(`Election now in state: ${states[await voteCon.methods.state().call({from: admin})]}\n`)
+    } else throw `Registration could not be ended`
+}
+
+async function subComm(voter) {
+    if (delegations[voter.address] != null) {//return if vote delegated
+        return
+    }
+    console.log(`Commitment for voter: ${voter.address}`)
+    await web3.eth.personal.unlockAccount(voter.address, "")
+    let conVoter = await voteCon.methods.getVoter().call({from: voter.address})
+    console.log(`Get voter (Registered keys, reconstructed keys, commitment): ${JSON.stringify(conVoter)}`)
+
+    //get voter registered and reconstructed keys
+    let xG = [conVoter[0][0], conVoter[0][1]]
+    let yG = [conVoter[1][0], conVoter[1][1]]
+    let zkp;
+
+    //todo create 1 out of 2 ZKP
+    if (noVotes.includes(voter.address)) {
+        zkp = await cryptoCon.methods.create1outof2ZKPNoVote(xG, yG, voter.w, voter.r, voter.d, voter.x).call({from: voter.address})
+    } else {
+        zkp = await cryptoCon.methods.create1outof2ZKPYesVote(xG, yG, voter.w, voter.r, voter.d, voter.x).call({from: voter.address})
+    }
+    // console.log(`One out of two ZKP: ${JSON.stringify(zkp)}`)
+
+    let y = [zkp[0][0], zkp[0][1]]
+    let a1 = [zkp[0][2], zkp[0][3]]
+    let b1 = [zkp[0][4], zkp[0][5]]
+    let a2 = [zkp[0][6], zkp[0][7]]
+    let b2 = [zkp[0][8], zkp[0][9]]
+    let params = [zkp[1][0], zkp[1][1], zkp[1][2], zkp[1][3]]
+
+    // verify 1 out of 2 ZKP
+    let index = await voteCon.methods.addressid(voter.address).call({from: voter.address})
+    let result = await voteCon.methods.verify1outof2ZKP(params, y, a1, b1, a2, b2, index).call({from: voter.address})
+    console.log(`ZKP verified: ${JSON.stringify(result)}\n`)
+
+    //submit committment
+    let hash = await cryptoCon.methods.commitToVote(params, xG, yG, y, a1, b1, a2, b2).call({from: voter.address})
+    await voteCon.methods.submitCommitment(hash).send({from: voter.address,gas: 4200000})
+}
+
+async function subDelComm(delegator) {
+    console.log(`Commitment for delegator: ${delegator}`)
+    await web3.eth.personal.unlockAccount(delegator, "")
+    let conDelegator = await voteCon.methods.getVoter().call({from: delegator})
+    console.log(`Get Delegator (Registered keys, reconstructed keys, commitment): ${JSON.stringify(conDelegator)}`)
+
+    let delegatee = delegations[delegator]
+    await web3.eth.personal.unlockAccount(delegatee, "")
+
+    //get delegatee's xG
+    let delegateeVals = voters.find(v => v.address === delegatee)
+    console.log(`Delegatee's xG: ${delegateeVals.xG}`)
+
+    //get Delegator keys
+    let delegatorVals = voters.find(v => v.address === delegator)
+
+    //get delegator reconstructed keys
+    let yG = [conDelegator[1][0], conDelegator[1][1]]
+
+    //todo create 1 out of 2 ZKP
+    let zkp;
+    if (noVotes.includes(delegatee)) {
+        zkp = await cryptoCon.methods.create1outof2ZKPNoVote(delegateeVals.xG, yG, delegatorVals.w, delegatorVals.r, delegatorVals.d, delegateeVals.x).call({from: delegatee})
+    } else {
+        zkp = await cryptoCon.methods.create1outof2ZKPYesVote(delegateeVals.xG, yG, delegatorVals.w, delegatorVals.r, delegatorVals.d, delegateeVals.x).call({from: delegatee})
+    }
+    // console.log(`One out of two ZKP: ${JSON.stringify(zkp)}`)
+
+    let y = [zkp[0][0], zkp[0][1]]
+    let a1 = [zkp[0][2], zkp[0][3]]
+    let b1 = [zkp[0][4], zkp[0][5]]
+    let a2 = [zkp[0][6], zkp[0][7]]
+    let b2 = [zkp[0][8], zkp[0][9]]
+    let params = [zkp[1][0], zkp[1][1], zkp[1][2], zkp[1][3]]
+
+    // verify 1 out of 2 ZKP
+    let delegatorIndex = await voteCon.methods.addressid(delegator).call({from: delegatee})
+    console.log(`Delegator index: ${delegatorIndex}`)
+    let result = await voteCon.methods.verify1outof2ZKP(params, y, a1, b1, a2, b2, delegatorIndex).call({from: delegatee})
+    console.log(`ZKP verified: ${JSON.stringify(result)}\n`)
+
+    //submit committment
+    let hash = await cryptoCon.methods.commitToVote(params, delegateeVals.xG, yG, y, a1, b1, a2, b2).call({from: delegatee})
+    await voteCon.methods.submitCommitment(hash, delegator).send({from: delegatee, gas: 4200000})
+}
+
+async function submitCommitments() {
+    //for each voter that has NOT delegated, generate the appropriate values for commitment and commit vote
+    for (const voter of voters){
+        await subComm(voter)
+    }
+
+    await web3.eth.personal.unlockAccount(admin, "")//unlock Admin account
+    console.log(`Num non-delegated commitments: ${await voteCon.methods.totalcommitted().call({from: admin})}`)
+
+    //for each voter that has delegated, generate the appropriate values for commitment and commit vote
+    for (const delegator of Object.keys(delegations)){
+        await subDelComm(delegator)
+    }
+
+    await web3.eth.personal.unlockAccount(admin, "")//unlock Admin account
+    console.log(`Num total commitments: ${await voteCon.methods.totalcommitted().call({from: admin})}`)
 }
 
 async function conductProtocol() {
@@ -241,39 +374,24 @@ async function conductProtocol() {
     console.log(`TOTAL REGISTERED: ${await voteCon.methods.totalregistered().call()}`)
     console.log("")
 
+    await registerDelegations()
+    console.log('Delegation completed\n')
 
-    // while(true) {
-    //     try {
-    //         let voterLogin = await web3.eth.personal.unlockAccount("0x828c81e01efab5a33417c4e3d1d24b5f521ec30a", "");
-    
-    //         let xBytes = secp256k1.utils.randomPrivateKey()
-    //         let xHex = secp256k1.utils.bytesToHex(xBytes)
-    //         let x = `0x${xHex}`
-    //         let xGPoint = secp256k1.Point.fromPrivateKey(xBytes)
-    //         let xG = [`0x${xGPoint.x.toString(16)}`,`0x${xGPoint.y.toString(16)}`]
-    //         let v = `0x${secp256k1.utils.bytesToHex(secp256k1.utils.randomPrivateKey())}`
-    
-    //         var zkp = await cryptoCon.methods.createZKP(x, v, xG).call({from: "0x828c81e01efab5a33417c4e3d1d24b5f521ec30a", gas: 4200000});
-    //         console.log(zkp)
-    //         break
-    //     } catch(e) {
-    //         console.log("ERROR")
-    //     }
-    // }
+    //wait till voter registration deadline is reached + buffer
+    console.log("WAITING FOR VOTER REGISTRATION DEADLINE...")
+    await new Promise(resolve => setTimeout(resolve, (deadlines.votersFinishSignup - Date.now()) + 1000))
+    await web3.eth.personal.unlockAccount(admin, "")//unlock Admin account
+    //we need a do nothing function to update block number and timestamp
+    await voteCon.methods.doNothing().send({from: admin})
+    console.log(`${deadlines.votersFinishSignup} vs ${Date.now()}`)
 
-    // let voterLogin = await web3.eth.personal.unlockAccount("0x828c81e01efab5a33417c4e3d1d24b5f521ec30a", "");
-    // let vZkp = await cryptoCon.methods.verifyZKP(
-    //     [
-    //         '0x284fa800c5250b202e80419a2c28d6409570260a577d45675a988e6a8d85509',
-    //         '0x3032c0aa081ebd19d378d2c8df656ec8a49016c0bd4c988a141256331b5205ac'
-    //     ],'54938706834021188046589744708636769752067740673705567682809120311266526672756',
-    //     [
-    //         '107687742604959417364442391574625660462039785437464552194143255727440619209578',
-    //         '95038889093454847267194781371975244360638832316923818587658057514680996717959',
-    //         '1'
-    //     ]).call({from: "0x828c81e01efab5a33417c4e3d1d24b5f521ec30a", gas: 4200000});
-    //     console.log('vZkp:')
-    //     console.log(vZkp)
+    //Admin finishes registration
+    await finishRegistration()
+
+    //create commitments
+    await submitCommitments()
+
+    //todo commitment, creating and verifying oneOutOfTwoZKPs(fix _mul), casting votes and delegates votes
 }
 
 await conductProtocol()
